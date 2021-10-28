7606c0e89d7b4cf706e4ff393858306edd8e4b2b
Jyothy Baby <jyothy.v.baby@gmail.com>
2021-10-28 18:38:48 -0400

    Adding .gitignore, adding a piece of code for heroku connection

--- server.js
+++ server.js
@@ -12,10 +12,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 //Database name is fitness
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness",
+ {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
-});
+
+}
+);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
